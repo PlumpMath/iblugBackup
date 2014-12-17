35c42091d46dc622422436ce614110710d437ec5
chidea <sbw228@gmail.com>
2014-12-18 04:15:04 +0900

    videofile backup support added

--- pyBatchDownloader.py
+++ pyBatchDownloader.py
@@ -7,7 +7,8 @@ from urllib.request import urlopen,urlretrieve #,build_opener,HTTPCookieProcesso
 from urllib.error import HTTPError
 from http.cookiejar import CookieJar
 from threading import Thread
-from os.path import join, splitext, exists
+from os.path import join, splitext, exists ,split
+from os import mkdir
 
 class BatchDownloader():
     def __init__(self, url, regex):
@@ -22,35 +23,70 @@ class BatchDownloader():
         #     print('error line:%d, msg:%s' % (e.lineno, e.msg))
         # self._dom = parseString(self._doc)
         self._find = findall(regex, self._doc)
-    def run(self,urlpre,re,fre,downpath):
-        # Thread(target=self._run,args=(urlpre,compile(re),compile(fre),downpath)).run()
-        self._run(urlpre,compile(re),compile(fre),downpath)
-    def _run(self,urlpre,re,fre,downpath):
-        log=open(join(downpath, 'fileList.txt'),'w')
-        for f in self._find:
+    def run(self,urlpre,fileUrlRE,downpath):
+        if not exists(downpath): mkdir(downpath)
+        # Thread(target=self._run,args=(urlpre,compile(fileUrlRE),compile(titleRE),downpath)).run()
+        self._run(urlpre,compile(fileUrlRE),downpath)
+    def _run(self,urlpre,fileUrlRE,downpath):
+        log=open(join(downpath, 'fileList.txt'), 'a')
+        for t,f,title in self._find:
             done=False
-            furl,title,fname='','',''
+            furl,fname='',''
             while not done:
                 try:
-                    playerDoc = urlopen(urlpre + f).readall().decode(errors='ignore')
-                    furl = re.search(playerDoc).group()
-                    title = unescape(fre.search(playerDoc).group().strip())
-                    fname = title.replace('?','？').replace('<','[').replace('>',']').replace('*','.').replace('"',"'") + splitext(furl)[1]
+                    playerDoc = urlopen(urlpre.format(t) + f).readall().decode(errors='ignore')
+                    furl = fileUrlRE.search(playerDoc).group()
+                    title = unescape(title)
+                     # title = unescape(titleRE.search(playerDoc).group().strip())
+                    fname = title.replace('?','？').replace('<','[').replace('>',']').replace('*','.').replace('"',"'") + splitext(furl if not t=='v' else split(furl)[0])[1]
                     for c in '\\/:*?"<>|': fname = fname.replace(c,'') # replace characters that are not applicable for file name # possible future replacement = '__;_!\'()l'
-                    fpath=join(downpath, fname)
-                    if not exists(fpath):
-                        urlretrieve(furl, fpath)
-                    # else:
-                        # return
+                    fpath = join(downpath, fname)
+                    if exists(fpath):
+                        done=None
+                    else:
+                        if t=='a':
+                            urlretrieve(furl, fpath)
+                        elif t=='v':
+                            urlretrieve(furl, 'tmp')
+                            furl = split(furl)[0]
+                            indexFname=''
+                            with open('tmp') as f:
+                                line=''
+                                while not line.startswith('index'): line = f.readline()
+                                indexFname=line
+                            urlretrieve('/'.join((furl,indexFname)), 'tmp')
+                            segments=[]
+                            with open('tmp') as f:
+                                line=' '
+                                while line != '':
+                                    line = f.readline()
+                                    if line.startswith('segment'): segments.append(line)
+                                indexFname=line
+                            for segment in segments:
+                                urlretrieve('/'.join((furl,segment)), 'tmp')
+                                with open('tmp','rb') as f, open(fpath,'ab') as w:
+                                    tmp=b' '
+                                    while tmp != b'':
+                                        tmp = f.read(1)
+                                        w.write(tmp)
                     done=True
                 except HTTPError as e:
                     print(e.code, e.msg)
                     sleep(3)
-            print(furl, '=',title,'saved as', fname)
-            log.write(furl+'\t'+title+'\t\t\t\t'+fname+'\n')
+            print(furl, '=',title,'saved as' if done is True else 'already exists as', fname)
+            if done is True: log.write(furl+'\t'+title+'\t\t\t\t'+fname+'\n')
         log.close()
 
 if __name__ == '__main__':
-    channel='birdfly' #'sciencewithpeople'
-    BatchDownloader('http://%s.iblug.com/index.jsp?limit_num=999999999' % channel, '(?<=png"  onClick="fn_listen\(\'/podcast/aplayer_jw.jsp\?lecture_cd=)[^\'"&]+') \
-        .run('http://%s.iblug.com/podcast/aplayer_jw.jsp?lecture_cd=' % channel, '(?<=file: \')[^\']+', '(?<=class="title">)[^<]+', 'E:\\iblug_backup\\%s' % channel)
+    from sys import argv
+    channel, workdir = '', ''
+    if len(argv) == 3:
+        channel, workdir = argv[1:3]
+    elif len(argv) == 2:
+        channel = argv[1]
+    else:
+        channel = input('channel name >')
+    while not exists(workdir): workdir = input('existing work path ("." for current dir)>')
+
+    BatchDownloader('http://%s.iblug.com/index.jsp?limit_num=999999999' % channel, '(?<=(?:jpg|png)"  onClick="fn_listen\(\'/podcast/(?P<type>[av])player_jw.jsp\?lecture_cd=)(?P<code>[^\'"&]+)(?:.{20,50}title=\')(?P<title>[^\']+)') \
+        .run('http://%s.iblug.com/podcast/{}player_jw.jsp?lecture_cd=' % channel, '(?<=file: \')[^\']+', join(workdir,channel))
